87e308052f5c185fa841ef0eb91a0344ba5f1a11
Anmol Jhamb <talktoanmol@outlook.com>
2023-12-07 18:24:06 +0530

    code added for controlling servo motors

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -47,20 +47,32 @@ void senseRfid()
 
 void setupServos()
 {
+    DDRB |= (1 << PB1) | (1 << PB2);                        // Set PB1 as output
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11); // Fast PWM, non-inverting mode // Setup for both OC1A, AND OC1B
+    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11);    // Fast PWM, prescaler = 8
+    ICR1 = 39999;                                           // 20ms PWM period
+
+    // Set both initially to be in the middle
+    OCR1A = 1999;
+    OCR1B = 1999;
 }
 
 void servo1Open()
 {
+    OCR1A = 2999;
 }
 
 void servo1Close()
 {
+    OCR1A = 1999;
 }
 
 void servo2Open()
 {
+    OCR2A = 2999;
 }
 
 void servo2Close()
 {
+    OCR2A = 1999;
 }
